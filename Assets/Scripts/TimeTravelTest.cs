9e475a1bc605c5db467a388764a9a035126a5f0d
Swibba42 <s.schuiten26@gmail.com>
2024-04-07 22:40:53 +0200

    Level Select 1

---
+++ Assets/Scripts/TimeTravelTest.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeTravelTest : MonoBehaviour
+{
+    public GameObject levelSelectCanvas;
+    public GameObject interactText;
+    public bool canvasActive = false;
+    public bool interactTextActive = false;
+
+
+    public void OnTriggerEnter (Collider other)
+    {
+        if (!canvasActive)
+            if (other.name == "Player")
+            {
+                interactText.SetActive(true);
+                interactTextActive = true;
+            }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            interactText.SetActive(false);
+            interactTextActive = false;
+        }
+    }
+
+    void Update()
+    {
+       if(interactTextActive && Input.GetKeyDown(KeyCode.F))
+        {
+            levelSelectCanvas.SetActive(true);
+            canvasActive = true;
+            Time.timeScale = 0f;
+        }
+
+       if(canvasActive)
+        {
+            interactText.SetActive(false);
+            interactTextActive = false;
+        }
+    }
+}
