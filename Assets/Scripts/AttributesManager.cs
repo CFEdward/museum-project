4fe3e433815669ed3ec7fdb9696423269470dfce
shaspog <vallio5104@gmail.com>
2024-04-08 10:00:28 +0200

    enemy healthbar

--- Assets/Scripts/AttributesManager.cs
+++ Assets/Scripts/AttributesManager.cs
@@ -1,15 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class AttributesManager : MonoBehaviour
 {
+    public event EventHandler<OnHitHealthChangedEventArgs> OnHit;
+    public class OnHitHealthChangedEventArgs : EventArgs
+    {
+        public float healthNormalized;
+    }
+
+    public int healthMax;
     public int health;
     public int attack;
+
+    public void Awake()
+    {
+        healthMax = health;
+    }
 
     public void TakeDamage(int amount)
     {
         health -= amount;
+        OnHit?.Invoke(this, new OnHitHealthChangedEventArgs
+        {
+            healthNormalized = (float)health / (float)healthMax,
+        });
     }
 
     public void DealDamage(GameObject target)
