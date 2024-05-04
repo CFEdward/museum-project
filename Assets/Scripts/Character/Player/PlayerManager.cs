42a07f7083057923c7a859488354ef04a84abf95
Eduard Ciofu <cfedward7@gmail.com>
2024-05-04 04:03:21 +0200

    basic light detection WIP

--- Assets/Scripts/Character/Player/PlayerManager.cs
+++ Assets/Scripts/Character/Player/PlayerManager.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class PlayerManager : MonoBehaviour
 {
     private InputManager inputManager;
     private CameraManager cameraManager;
     private PlayerLocomotion playerLocomotion;
+
+    public float lightLevel;
 
     private void Awake()
     {
@@ -18,6 +21,13 @@ public class PlayerManager : MonoBehaviour
     private void Update()
     {
         inputManager.HandleAllInputs();
+
+        //SphericalHarmonicsL2 harmonics = new SphericalHarmonicsL2();
+        //LightProbes.GetInterpolatedProbe(gameObject.transform.position, GetComponent<Renderer>(), out harmonics);
+        //lightLevel = (0.2989f * harmonics[0, 0]) + (0.587f * harmonics[1, 0]) + (0.114f * harmonics[2, 0]);
+
+        //if (lightLevel < 10f) Debug.Log("Hidden");
+        //else Debug.Log("Visible");
     }
 
     private void FixedUpdate()
---
+++ Assets/Scripts/Mechanics/LightDetection.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightDetection : MonoBehaviour
+{
+    [Header("Settings")]
+    [Tooltip("The camera that scans for light.")]
+    public Camera camLightScan;
+    [Tooltip("Show the light value in the log.")]
+    public bool bLogLightValue = false;
+    [Tooltip("Time between light value updates (default = 0.1f).")]
+    public float updateTime = 0.1f;
+
+    public static float lightValue;
+
+    private const int textureSize = 1;
+
+    private Texture2D texLight;
+    private RenderTexture texTemp;
+    private Rect rectLight;
+    private Color lightPixel;
+
+    private void Start()
+    {
+        StartLightDetection();
+    }
+
+    /// <summary>
+    /// Prepare all needed variables and start the light detection coroutine.
+    /// </summary>
+    private void StartLightDetection()
+    {
+        texLight = new Texture2D(textureSize, textureSize, TextureFormat.RGB24, false);
+        texTemp = new RenderTexture(textureSize, textureSize, 24);
+        rectLight = new Rect(0f, 0f, textureSize, textureSize);
+
+        StartCoroutine(LightDetectionUpdate(updateTime));
+    }
+
+    /// <summary>
+    /// Updates the light value each x seconds.
+    /// </summary>
+    /// <param name="_updateTime">Time in seconds between updates.</param>
+    /// <returns></returns>
+    private IEnumerator LightDetectionUpdate(float _updateTime)
+    {
+        while (true)
+        {
+            //Set the target texture of the cam.
+            camLightScan.targetTexture = texTemp;
+            //Render into the set target texture.
+            camLightScan.Render();
+
+            //Set the target texture as the active rendered texture.
+            RenderTexture.active = texTemp;
+            //Read the active rendered texture.
+            texLight.ReadPixels(rectLight, 0, 0);
+
+            //Reset the active rendered texture.
+            RenderTexture.active = null;
+            //Reset the target texture of the cam.
+            camLightScan.targetTexture = null;
+
+            //Read the pixel in middle of the texture.
+            lightPixel = texLight.GetPixel(textureSize / 2, textureSize / 2);
+
+            //Calculate light value, based on color intensity (from 0f to 1f).
+            lightValue = (lightPixel.r + lightPixel.g + lightPixel.b) / 3f;
+
+            if (bLogLightValue)
+            {
+                Debug.Log("Light Value: " + lightValue);
+            }
+
+            yield return new WaitForSeconds(_updateTime);
+        }
+    }
+}
