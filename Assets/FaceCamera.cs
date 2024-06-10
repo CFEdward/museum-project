409364648b366679493aeb2479aa3269ad736449
Eduard Ciofu <cfedward7@gmail.com>
2024-06-10 05:09:36 +0200

    made collectibles better looking

---
+++ Assets/FaceCamera.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class FaceCamera : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Camera.main.transform.rotation;
+    }
+}
--- Assets/Scripts/Collectibles/CollectibleRotate.cs
+++ Assets/Scripts/Collectibles/CollectibleRotate.cs
@@ -1,10 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CollectibleRotate : MonoBehaviour
 {
-
     // Update is called once per frame
     void Update()
     {
